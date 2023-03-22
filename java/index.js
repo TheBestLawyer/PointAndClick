da127fb1aa835ec804863d2a7e87c506ea45db51
TheBestLawyer <u.kovalkovas@sintlucasedu.nl>
2023-03-22 16:08:30 +0100

    yip yup

--- java/index.js
+++ java/index.js
@@ -1,1 +1,15 @@
 document.getElementById("mainTitle").innerText = "Point and click adventure"
+
+const gameWindow = document.getElementById("gameWindow");
+const mainCharacter = document.getElementById("mainCharacter");
+
+gameWindow.onclick = function(e) {
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left
+    var y = e.clientY - rect.top
+    mainCharacter.style.left = x+"px";
+    mainCharacter.style.top = y+"px";
+    
+    
+    console.log(x + " " + y);
+}
