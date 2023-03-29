e7e7c927c919ec3df8ab5e0bfdffc54fb0ceef6b
TheBestLawyer <u.kovalkovas@sintlucasedu.nl>
2023-03-29 16:06:06 +0200

    did stuff

--- java/index.js
+++ java/index.js
@@ -1,5 +1,6 @@
 document.getElementById("mainTitle").innerText = "Point and click adventure"
 
+const offsetCharacter = 16;
 const gameWindow = document.getElementById("gameWindow");
 const mainCharacter = document.getElementById("mainCharacter");
 
@@ -7,6 +8,7 @@ gameWindow.onclick = function(e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left
     var y = e.clientY - rect.top
+
     mainCharacter.style.left = x - offsetCharacter + "px";
     mainCharacter.style.top = y - offsetCharacter + "px";
     
